0b7c5f9903d8ddd88b868bf58489243ee74e00d2
junwei17 <wongjunwei17@gmail.com>
2023-01-16 17:08:06 +0800

    Created basic layout, template, built on some preexisting auth codes, created firebase and hooked onto the react native app, create login with signup forms

---
+++ LSDIPreactNative/config/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import Constants from 'expo-constants';
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: Constants.manifest.extra.apiKey,
+  authDomain: Constants.manifest.extra.authDomai,
+  projectId: Constants.manifest.extra.projectId,
+  storageBucket: Constants.manifest.extra.storageBucket,
+  messagingSenderId: Constants.manifest.extra.messagingSenderId,
+  appId: Constants.manifest.extra.appId,
+  databaseURL: Constants.manifest.extra.databaseURL
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const database = getFirestore();
---
+++ LSDIPreactNative/screens/Home.js
@@ -0,0 +1,98 @@
+import React, { useEffect } from "react";
+import { View, TouchableOpacity, Text, Image, StyleSheet, FlatList } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { FontAwesome } from '@expo/vector-icons';
+import colors from '../colors';
+import { Entypo } from '@expo/vector-icons';
+//const catImageUrl = "https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=49ed3252c0b2ffb49cf8b508892e452d";
+
+const DATA = [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'First Item',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'Second Item',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Third Item',
+    },
+  ];
+  
+const Item = ({title}) => (
+    <View style={styles.item}>
+        <Text style={styles.title}>{title}</Text>
+    </View>
+);
+
+const Home = () => {
+
+    const navigation = useNavigation();
+
+    useEffect(() => {
+        navigation.setOptions({
+            headerLeft: () => (
+                <FontAwesome name="home" size={24} color={colors.gray} style={{marginLeft: 15}}/>
+            ),
+            headerRight: () => (
+                <Image
+                    //source={{ uri: catImageUrl }}
+                    source = {require('../assets/washin.jpg')}
+                    style={{
+                        width: 40,
+                        height: 40,
+                        marginRight: 15,
+                    }}
+                />
+            ),
+        });
+    }, [navigation]);
+
+    return (
+        <View>
+            <FlatList
+                data={DATA}
+                renderItem={({item}) => <Item title={item.title} />}
+                keyExtractor={item => item.id}
+            />
+            <View style={styles.container}>
+                <TouchableOpacity 
+                    onPress={() => navigation.navigate("Chat")}
+                    style={styles.chatButton}
+                >
+                    <Entypo name="chat" size={24} color={colors.lightGray} />
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+    };
+
+    export default Home;
+
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            justifyContent: 'flex-end',
+            alignItems: 'flex-end',
+            backgroundColor: "#fff",
+        },
+        chatButton: {
+            backgroundColor: colors.primary,
+            height: 50,
+            width: 50,
+            borderRadius: 25,
+            alignItems: 'center',
+            justifyContent: 'center',
+            shadowColor: colors.primary,
+            shadowOffset: {
+                width: 0,
+                height: 2,
+            },
+            shadowOpacity: .9,
+            shadowRadius: 8,
+            marginRight: 20,
+            marginBottom: 50,
+        }
+    });
