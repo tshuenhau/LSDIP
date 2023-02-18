f8201854306fdfb884a56bdbf5e4c91c6373f78b
junwei17 <77657709+junwei17@users.noreply.github.com>
2023-02-19 01:31:16 +0800

    fix on web codes v1

--- LSDIPreactNative/App.js
+++ LSDIPreactNative/App.js
@@ -63,6 +63,38 @@ function CustomDrawerContent(props) {
   );
 }
 
+// function OperationStack() {
+//   return (
+//     <Drawer.Navigator
+//       useLegacyImplementation
+//       drawerContent={(props) => <CustomDrawerContent {...props} />}
+//     >
+//       <Drawer.Group>
+//         <Drawer.Screen name='Home' component={Home} />
+//         <Drawer.Screen name='Staff' component={Staff} />
+//         <Drawer.Screen name='Admin' component={Admin} />
+//         <Drawer.Screen name='Driver' component={Driver} />
+//         <Drawer.Screen name='Chat' component={Chat} />
+//       </Drawer.Group>
+//       <Drawer.Group screenOptions={{ presentation: 'modal' }}>
+//         <Drawer.Screen name='CreateOutlet' component={CreateOutlet} />
+//       </Drawer.Group>
+//     </Drawer.Navigator>
+//   );
+// }
+
+// function AuthStack() {
+//   return (
+//     <Stack.Navigator screenOptions={{ headerShown: false }}>
+//       <Stack.Screen name='Login' component={Login} />
+//       <Stack.Screen name='Signup' component={Signup} />
+//       <Stack.Screen name='Admin' component={Admin} />
+//       <Stack.Screen name='Staff' component={Staff} />
+//       <Stack.Screen name='Driver' component={Driver} />
+//     </Stack.Navigator>
+//   );
+// }
+
 function RootNavigator() {
   const { user, setUser } = useContext(AuthenticatedUserContext);
   const [isLoading, setIsLoading] = useState(true);
@@ -99,27 +131,15 @@ function RootNavigator() {
       </View>
     );
   }
-  
-  const drawerContent = (props) => {
-    return (
-      <DrawerContentScrollView {...props}>
-        <DrawerItemList {...props} />
-        <DrawerItem
-          label="Sign Out"
-          onPress={handleSignOut}
-        />
-        <DrawerItem
-          label="Close Drawer"
-          onPress={() => props.navigation.closeDrawer()}
-        />
-      </DrawerContentScrollView>
-    );
-  }
-
+  //console.log(user?.metadata?.customClaims);
   if (user1?.role === "Admin") {
     return (
       <NavigationContainer>
-        <Drawer.Navigator drawerContent={drawerContent}>
+        <Drawer.Navigator
+          useLegacyImplementation
+          drawerContent={(props) => <CustomDrawerContent {...props} />}
+        >
+          {/* <Drawer.Group> */}
           <Drawer.Screen name='Home' component={Home} />
           <Drawer.Screen name='My Profile' component={MyProfile} />
           <Drawer.Screen name='Staff' component={Staff} />
@@ -128,26 +148,40 @@ function RootNavigator() {
           <Drawer.Screen name='Delivery' component={Delivery} />
           <Drawer.Screen name='Driver' component={Driver} />
           <Drawer.Screen name='Chat' component={Chat} />
+          {/* </Drawer.Group> */}
+          {/* <Drawer.Group screenOptions={{ presentation: 'modal' }}>
+            <Drawer.Screen name='CreateOutlet' component={CreateOutlet} />
+          </Drawer.Group> */}
         </Drawer.Navigator>
       </NavigationContainer >
     );
   } else if (user1?.role === "Staff") {
     return (
       <NavigationContainer>
-        <Drawer.Navigator drawerContent={drawerContent}>
-          <Drawer.Screen name='Home' component={Home} />
-          <Drawer.Screen name='Staff' component={Staff} />
-          <Drawer.Screen name='Chat' component={Chat} />
+        <Drawer.Navigator
+          useLegacyImplementation
+          drawerContent={(props) => <CustomDrawerContent {...props} />}
+        >
+          <Drawer.Group>
+            <Drawer.Screen name='Home' component={Home} />
+            <Drawer.Screen name='Staff' component={Staff} />
+            <Drawer.Screen name='Chat' component={Chat} />
+          </Drawer.Group>
         </Drawer.Navigator>
       </NavigationContainer>
     );
   } else if (user1?.role === "Driver") {
     return (
       <NavigationContainer>
-        <Drawer.Navigator drawerContent={drawerContent}>
-          <Drawer.Screen name='Home' component={Home} />
-          <Drawer.Screen name='Driver' component={Driver} />
-          <Drawer.Screen name='Chat' component={Chat} />
+        <Drawer.Navigator
+          useLegacyImplementation
+          drawerContent={(props) => <CustomDrawerContent {...props} />}
+        >
+          <Drawer.Group>
+            <Drawer.Screen name='Home' component={Home} />
+            <Drawer.Screen name='Driver' component={Driver} />
+            <Drawer.Screen name='Chat' component={Chat} />
+          </Drawer.Group>
         </Drawer.Navigator>
       </NavigationContainer>
     );
@@ -164,4 +198,77 @@ function RootNavigator() {
       </NavigationContainer >
     )
   }
+}
+
+// function RootNavigator() {
+//   const {user, setUser} = useContext(AuthenticatedUserContext);
+//   const [isLoading, setIsLoading] = useState(true);
+
+//   useEffect(() => {
+//     const unsubscribeAuth = onAuthStateChanged(auth, async authenticatedUser => {
+//       authenticatedUser ? setUser(authenticatedUser) : setUser(null);
+//       setIsLoading(false);
+//     });
+//     return unsubscribeAuth;
+//   }, [user]);
+
+//   if (isLoading) {
+//     return (
+//       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//         <ActivityIndicator size='large' />
+//       </View>
+//     );
+//   }
+
+//   // Get the user's role from the Firebase user object
+//   const userRole = user?.metadata?.customClaims?.role;
+
+//   // Render different stack navigators based on the user's role
+//   if (userRole === "Admin") {
+//     return (
+//       <NavigationContainer>
+//         <Stack.Navigator>
+//           <Stack.Screen name="Admin" component={Admin} />
+//           <Stack.Screen name="Chat" component={Chat} />
+//         </Stack.Navigator>
+//       </NavigationContainer>
+//     );
+//   } else if (userRole === "Staff") {
+//     return (
+//       <NavigationContainer>
+//         <Stack.Navigator>
+//           <Stack.Screen name="Staff" component={Staff} />
+//           <Stack.Screen name="Chat" component={Chat} />
+//         </Stack.Navigator>
+//       </NavigationContainer>
+//     );
+//   } else if (userRole === "Driver") {
+//     return (
+//       <NavigationContainer>
+//         <Stack.Navigator>
+//           <Stack.Screen name="Driver" component={Driver} />
+//           <Stack.Screen name="Chat" component={Chat} />
+//         </Stack.Navigator>
+//       </NavigationContainer>
+//     );
+//   } else {
+//     // User has no role or an invalid role
+//     return (
+//       <NavigationContainer>
+//         <Stack.Navigator>
+//           <Stack.Screen name="Home" component={Home} />
+//           <Stack.Screen name="Chat" component={Chat} />
+//         </Stack.Navigator>
+//       </NavigationContainer>
+//     );
+//   }
+// }
+
+
+export default function App() {
+  return (
+    <AuthenticatedUserProvider>
+      <RootNavigator />
+    </AuthenticatedUserProvider>
+  );
 }
