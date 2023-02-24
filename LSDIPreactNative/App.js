4c83bedf1a3bd4cf47b3ab56141c98ca913c1b25
seancxy <seanchengxy13@gmail.com>
2023-02-24 10:36:18 +0800

    dependency stuff that should have been fixed the previous commit

--- LSDIPreactNative/App.js
+++ LSDIPreactNative/App.js
@@ -1,14 +1,20 @@
 import 'react-native-gesture-handler';
-import { Navigation } from 'react-native-navigation';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ActivityIndicator, TouchableOpacity, SafeAreaView, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 import React, { useState, createContext, useContext, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from '@react-navigation/drawer';
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from './config/firebase';
 import Login from './screens/Login';
 import Signup from './screens/Signup';
+// import Chat from './screens/Chat';
 import Home from './screens/Home';
 import OutletManagement from './screens/OutletManagement';
 import AdminRostering from './screens/AdminRostering';
@@ -19,22 +25,10 @@ import MyProfile from './screens/MyProfile';
 import Delivery from './screens/Delivery';
 import ForgotPassword from './screens/ForgotPassword';
 import { firebase } from "./config/firebase";
-import Icon from 'react-native-vector-icons/FontAwesome';
 
-
-// Register all the screens
-Navigation.registerComponent('Login', () => Login);
-Navigation.registerComponent('Signup', () => Signup);
-Navigation.registerComponent('Home', () => Home);
-Navigation.registerComponent('Admin', () => Admin);
-Navigation.registerComponent('AdminTimeslots', () => AdminTimeslots);
-Navigation.registerComponent('StaffRostering', () => StaffRostering);
-Navigation.registerComponent('Driver', () => Driver);
-Navigation.registerComponent('MyProfile', () => MyProfile);
-Navigation.registerComponent('Delivery', () => Delivery);
-Navigation.registerComponent('ForgotPassword', () => ForgotPassword);
-let isEnabled = true;
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
 const AuthenticatedUserContext = createContext({});
 
 const AuthenticatedUserProvider = ({ children }) => {
@@ -46,37 +40,15 @@ const AuthenticatedUserProvider = ({ children }) => {
   );
 };
 
-// Define the side menu component
-const SideMenu = ({ navigation, user }) => {
-  const [isCollapsed, setIsCollapsed] = useState(true);
-  console.log(isEnabled);
-  const handleSignOut = () => {
-    isEnabled = false;
-    auth.signOut()
-      .then(() => {
-        navigation.navigate('Login');
-      })
-      .catch(error => alert(error.message))
-  }
+const handleSignOut = () => {
+  auth.signOut()
+    .then(() => {
+      navigation.navigate('Login')
+    })
+    .catch(error => alert(error.message))
+}
 
-  if (!isEnabled) {
-    // Return null if user is not defined
-    return null;
-  }
-
-  const CollapsibleButton = ({ onPress }) => {
-    return (
-      <TouchableOpacity style={styles.collapsibleButton} onPress={onPress}>
-        <Icon name="bars" size={24} color="black" />
-      </TouchableOpacity>
-    );
-  };
-  
-
-  const handleCollapsibleButtonPress = () => {
-    setIsCollapsed(!isCollapsed);
-  };
-  
+function CustomDrawerContent(props) {
   return (
     <DrawerContentScrollView {...props}>
       <DrawerItemList {...props} />
@@ -97,25 +69,22 @@ function RootNavigator() {
   const [isLoading, setIsLoading] = useState(true);
   const auth1 = firebase.auth;
   const firestore = firebase.firestore;
-  const [user1, setUser1] = useState(null);
-  const navigationRef = React.useRef(null);
-
+  const [user1, setUser1] = useState(null)
   useEffect(() => {
     const unsubscribeAuth = auth1().onAuthStateChanged(
       async authenticatedUser => {
         if (authenticatedUser) {
-          firestore()
-            .collection('users')
-            .doc(auth1().currentUser.uid)
-            .get()
+          firestore().collection("users").doc(auth1().currentUser.uid).get()
             .then(user => {
               setUser1(user.data());
-              const userRole = user.data().role;
+              console.log(user);
+              console.log(user1?.role);
+              const userRole = user?.role;
               setUser({ ...authenticatedUser, role: userRole });
-            });
+            })
+
         } else {
           setUser(null);
-          setUser1(null);
         }
         setIsLoading(false);
       }
@@ -192,56 +161,9 @@ function RootNavigator() {
         </Stack.Navigator>
       </NavigationContainer >
     )
-  } else {
-    isEnabled = true;
-    return (
-      <AuthenticatedUserProvider>
-        <NavigationContainer ref={navigationRef}>
-          <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen name='Login' component={Login} />
-            <Stack.Screen name='Signup' component={Signup} />
-            <Stack.Screen name='ForgotPassword' component={ForgotPassword} />
-            <Stack.Screen name='Home' component={Home} />
-            <Stack.Screen name='Admin' component={Admin} />
-            <Stack.Screen name='Driver' component={Driver} />
-            <Stack.Screen name='MyProfile' component={MyProfile} />
-            <Stack.Screen name='Delivery' component={Delivery} />
-            <Stack.Screen name='StaffRostering' component={StaffRostering} />
-            <Stack.Screen name='AdminTimeslots' component={AdminTimeslots} />
-          </Stack.Navigator>
-          <SideMenu navigation={navigationRef.current} user={user1}/>
-        </NavigationContainer >
-      </AuthenticatedUserProvider>
-    );
   }
 }
 
-
-
-const styles = StyleSheet.create({
-  sideMenuItem: {
-    padding: 10,
-    alignItems: 'center',
-  },
-  sideMenuItemText: {
-    fontSize: 18,
-  },
-  collapsibleButton: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    padding: 10,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    borderTopWidth: 1,
-    borderTopColor: '#ccc',
-  },
-  collapsibleButtonText: {
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-});
 
 export default function App() {
   return (
