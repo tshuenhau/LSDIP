3d1c9988f7053fffc6a747981aca9ece3d8203ea
seancxy <seanchengxy13@gmail.com>
2023-03-08 17:55:11 +0800

    saving progress

--- LSDIPreactNative/screens/CreateOrder.js
+++ LSDIPreactNative/screens/CreateOrder.js
@@ -4,14 +4,23 @@ import {
     Image,
     StyleSheet,
     ScrollView,
+    TextInput,
+    Modal,
+    TouchableOpacity,
 } from 'react-native'
 import React, { useState, useEffect } from "react";
-import { firebase } from "../config/firebase";
 import Btn from "../components/Button";
+import TextBox from "../components/TextBox";
+import colors from '../colors';
+import { firebase } from "../config/firebase";
 
 export default function CreateOrder() {
 
     const [laundryItems, setLaundryItems] = useState([]);
+    const [laundryCategories, setLaundryCategories] = useState([]);
+    const [createModalData, setCreateModalData] = useState({});
+    const [createModalVisible, setCreateModalVisible] = useState(false);
+    const [searchQuery, setSearchQuery] = useState("");
     const [cart, setCart] = useState([]);
     const [totalAmount, setTotalAmount] = useState(0);
 
@@ -33,6 +42,18 @@ export default function CreateOrder() {
                 })
                 setLaundryItems(laundryItems)
             })
+
+        const laundryCategory = firebase.firestore().collection('laundryCategory');
+        laundryCategory
+            .get()
+            .then(querySnapshot => {
+                const laundryCategories = [];
+                querySnapshot.forEach(doc => {
+                    const { serviceName } = doc.data();
+                    laundryCategories.push(serviceName);
+                })
+                setLaundryCategories(laundryCategories);
+            })
     }, [])
 
     const addToCart = () => {
@@ -43,30 +64,55 @@ export default function CreateOrder() {
         console.log("todo remove from cart");
     }
 
+    const handleItemClick = (laundryItem) => {
+        console.log(laundryItem);
+        console.log("here");
+        // const [laundryItemName, typeOfServices, price, pricingMethod] = laundryItem;
+        setCreateModalData(laundryItem);
+        setCreateModalVisible(true);
+    }
+
+    const filteredLaundryItemList = laundryItems.filter((laundryItem) =>
+        laundryItem.laundryItemName.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+
     return (
-        <View style={styles.orderPage}>
-            <View style={styles.buttonsContainer}>
-                {laundryItems.map((laundryItem, key) =>
-                    <View key={key} >
-                        <View onClick={() => addToCart()}>
-                            <Text>{laundryItem.laundryItemName}</Text>
-                            {/* to store url of icon in db */}
-                            <Image source={'https://picsum.photos/200'} style={styles.image} />
-                            <Text>${laundryItem.price}</Text>
-                        </View>
+        <View>
+            <View style={styles.orderPage}>
+                <View style={styles.buttonsContainer}>
+                    <View style={styles.searchContainer}>
+                        <TextInput
+                            style={styles.searchInput}
+                            value={searchQuery}
+                            onChangeText={setSearchQuery}
+                            placeholder="Search laundry item"
+                        />
+                    </View>
+                    <View>
 
                     </View>
-                )}
-            </View>
-            <View style={styles.totalContainer}>
-                <View style={styles.tableHeader}>
-                    <Text style={styles.tableHeaderText}>Item Type</Text>
-                    <Text style={styles.tableHeaderText}>Description</Text>
-                    <Text style={styles.tableHeaderText}>Item Name</Text>
-                    <Text style={styles.tableHeaderText}>Price</Text>
+                    {filteredLaundryItemList.map((laundryItem, key) =>
+                        <View key={key} style={styles.container} >
+                            <TouchableOpacity style={styles.card_template} onPress={() => handleItemClick(laundryItem)}>
+                                {/* to store url of icon in db */}
+                                <Image source={'https://picsum.photos/200'} style={styles.card_image} />
+                                <View style={styles.text_container}>
+                                    <Text style={styles.card_title}>{laundryItem.typeOfServices} {laundryItem.laundryItemName}</Text>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                    )}
                 </View>
+                <View style={styles.totalContainer}>
+                    <View style={styles.tableHeader}>
+                        <Text style={styles.tableHeaderText}>Item Type</Text>
+                        <Text style={styles.tableHeaderText}>Description</Text>
+                        <Text style={styles.tableHeaderText}>Item Name</Text>
+                        <Text style={styles.tableHeaderText}>Price</Text>
+                    </View>
 
-                {/* {cart.map((item, index) => (
+                    {/* todo cart display */}
+                    {/* {cart.map((item, index) => (
                     <View key={index} style={styles.tableRow}>
                         <Text style={styles.tableRowText}>item type</Text>
                         <Text style={styles.tableRowText}>item desc</Text>
@@ -80,13 +126,112 @@ export default function CreateOrder() {
                         />
                     </View>
                 ))} */}
-                <Btn onClick={() => addToCart()} title="Checkout" style={{ width: "48%", margin: 5 }} />
+                    <Btn onClick={() => addToCart()} title="Checkout" style={{ width: "48%", margin: 5 }} />
+                </View>
             </View>
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={createModalVisible}
+            >
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <View style={styles.view}>
+                            <Text style={{ fontSize: 34, fontWeight: "800", marginBottom: 20 }}>Add to Cart</Text>
+                            <Text style={styles.itemText}> {createModalData.typeOfServices} {createModalData.laundryItemName} </Text>
+                            {/* <Text style={styles.itemText}>Type of Service:  </Text> */}
+                            <Text style={styles.itemText}>Pricing Method: {createModalData.pricingMethod} </Text>
+                            <TextBox placeholder="Description" onChangeText={text => handleChange(text, "description")} defaultValue={createModalData.description} />
+                            <Text style={styles.itemText}>Input price: </Text>
+                            <TextBox placeholder="Price" onChangeText={text => handleChange(text, "price")} defaultValue={createModalData.price} />
+                            <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center", width: "92%" }}>
+                                <Btn onClick={() => addToCart()} title="Add" style={{ width: "48%" }} />
+                                <Btn onClick={() => setCreateModalVisible(false)} title="Dismiss" style={{ width: "48%", backgroundColor: "#344869" }} />
+                            </View>
+                        </View>
+                    </View>
+                </View>
+            </Modal>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    searchInput: {
+        height: 40,
+        borderWidth: 1,
+        borderRadius: 5,
+        borderColor: colors.gray,
+        paddingHorizontal: 10,
+        fontSize: 18,
+        backgroundColor: colors.white,
+        marginVertical: 10,
+    },
+    cardButtons: {
+        flexDirection: "row",
+        justifyContent: 'space-between',
+    },
+    searchContainer: {
+        justifyContent: "center",
+        alignContent: "center",
+        width: "96%",
+    },
+    container: {
+        margin: 10,
+    },
+    card_template: {
+        width: 250,
+        height: 250,
+        boxShadow: "10px 10px 17px -12px rgba(0,0,0,0.75)"
+    },
+    card_image: {
+        width: 250,
+        height: 250,
+        borderRadius: 10
+    },
+    text_container: {
+        position: "absolute",
+        width: 250,
+        height: 30,
+        bottom: 0,
+        padding: 5,
+        backgroundColor: "rgba(0,0,0, 0.3)",
+        borderBottomLeftRadius: 10,
+        borderBottomRightRadius: 10
+    },
+    card_title: {
+        color: "white",
+    },
+    view: {
+        width: "100%",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    itemText: {
+        flex: 1,
+        fontSize: 16,
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 22,
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        padding: 35,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+    },
     orderPage: {
         flexDirection: 'row',
         justifyContent: 'space-evenly',
---
+++ LSDIPreactNative/screens/LaundryItemOld.js
@@ -0,0 +1,293 @@
+import React, { useState, useEffect } from "react";
+import {
+    View,
+    TouchableOpacity,
+    Text,
+    StyleSheet,
+    Modal,
+    UIManager,
+    Platform,
+    ScrollView,
+} from "react-native";
+import { FontAwesome } from '@expo/vector-icons';
+import TextBox from "../components/TextBox";
+import Btn from "../components/Button";
+import colors from '../colors';
+import { firebase } from "../config/firebase";
+import { doc, addDoc, getFirestore, collection, getDoc, getDocs, QuerySnapshot, deleteDoc, GeoPoint, updateDoc } from "firebase/firestore";
+import { SelectList } from 'react-native-dropdown-select-list'
+import LaundryList from "../components/LaundryItemList";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
+if (
+    Platform.OS === 'android' &&
+    UIManager.setLayoutAnimationEnabledExperimental
+) {
+    UIManager.setLayoutAnimationEnabledExperimental(true);
+}
+
+export default function LaundryItem({ navigation }) {
+
+    const [modalVisible, setModalVisible] = useState(false);
+    const [values, setValues] = useState(initialValues);
+    const laundryItem = firebase.firestore().collection('laundryItem');
+    const laundryCItem = firebase.firestore().collection('laundryCategory');
+    const [data, setData] = useState([]);
+    const [user, setUser] = useState(false);
+    const db = firebase.firestore()
+
+    useEffect(() => {
+        try {
+            const getUserId = async () => {
+                try {
+                    const id = await AsyncStorage.getItem('userId');
+                    if (id !== null) {
+                        getDoc(doc(db, "users", id)).then(docData => {
+                            if (docData.exists()) {
+                                //console.log(docData.data())
+                                setUser(docData.data())
+                            }
+                        })
+                    }
+                } catch (e) {
+                    console.log(e);
+                }
+            }
+
+            getUserId();
+        } catch (e) {
+            console.log("User Id does not exist in DB")
+        }
+    }, [])
+
+    const pricingMethods = [
+        { key: '1', value: 'Flat' },
+        { key: '2', value: 'Range' },
+        { key: '3', value: 'Weight' },
+    ]
+
+    useEffect(() => {
+        laundryCItem.onSnapshot(querySnapshot => {
+            const data = [];
+            querySnapshot.forEach(doc => {
+                const { serviceName } = doc.data();
+                data.push({
+                    key: doc.id,
+                    value: serviceName,
+                });
+            });
+            setData(data);
+        });
+    }, []);
+
+    //for laundry Item
+    const initialValues = {
+        typeOfServices: "",
+        laundryItemName: "",
+        pricingMethod: "",
+        price: "",
+    };
+
+    //for laundry item
+    const clearState = () => {
+        setValues({ ...initialValues });
+    }
+
+    //for laundry items
+    function handleChange(text, eventName) {
+        setValues(prev => {
+            return {
+                ...prev,
+                [eventName]: text
+            }
+        })
+    }
+
+    function createLaundryItem() {
+        laundryItem.add(values)
+            .then(() => {
+                setModalVisible(!modalVisible);
+                clearState;
+                console.log("Success");
+            }).catch((err) => {
+                console.log(err);
+            })
+    }
+
+    return (
+        <ScrollView>
+            <View>
+                {/*for create laundry item*/}
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={modalVisible}
+                >
+                    <View style={styles.centeredView}>
+                        <View style={styles.modalView}>
+                            <View style={styles.view}>
+                                <Text style={{ fontSize: 34, fontWeight: "800", marginBottom: 20 }}>Create New Laundry Item</Text>
+                                <TextBox placeholder="Laundry Item Name" onChangeText={text => handleChange(text, "laundryItemName")} />
+                                <View style={{
+                                    // height: 42,
+                                    width: "92%",
+                                    borderRadius: 25,
+                                    marginTop: 20
+                                }}>
+                                    <SelectList
+                                        data={data}
+                                        placeholder="Choose service"
+                                        searchPlaceholder="Search service"
+                                        setSelected={(val) => handleChange(val, "typeOfServices")}
+                                        save="value"
+                                    />
+                                </View>
+                                <View style={{
+                                    // height: 42,
+                                    width: "92%",
+                                    borderRadius: 25,
+                                    marginTop: 20
+                                }}>
+                                    <SelectList
+                                        data={pricingMethods}
+                                        placeholder="Choose pricing method"
+                                        searchPlaceholder="Search pricing method"
+                                        setSelected={(val) => handleChange(val, "pricingMethod")}
+                                        save="value"
+                                    />
+                                </View>
+                                <TextBox placeholder="Price" onChangeText={text => handleChange(text, "price")} />
+                                <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center", width: "92%" }}>
+                                    <Btn onClick={() => createLaundryItem()} title="Create" style={{ width: "48%" }} />
+                                    <Btn onClick={() => setModalVisible(!modalVisible)} title="Dismiss" style={{ width: "48%", backgroundColor: "#344869" }} />
+                                </View>
+                            </View>
+                        </View>
+                    </View>
+                </Modal >
+
+                <View style={styles.view2}>
+                    <TouchableOpacity
+                        onPress={() => setModalVisible(!modalVisible)}
+                        style={styles.btn}>
+                        <Text style={styles.text}>Create Laundry Item</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <View>
+                    <Text style={styles.listtext}>Laundry Item List</Text>
+                    <LaundryList />
+                </View>
+
+            </View >
+        </ScrollView>
+    )
+}
+
+const styles = StyleSheet.create({
+    cardBody: {
+        padding: 16,
+    },
+    itemContainer: {
+        backgroundColor: colors.lightGray,
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: "center",
+        paddingVertical: 8,
+        paddingRight: 20,
+    },
+    itemText: {
+        flex: 1,
+        fontSize: 16,
+    },
+    card: {
+        backgroundColor: '#fff',
+        marginVertical: 10,
+        marginHorizontal: 16,
+        borderRadius: 10,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        elevation: 3,
+    },
+    outletName: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    cardHeader: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 16,
+    },
+    deleteIcon: {
+        fontSize: 25,
+    },
+    view: {
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 20,
+    },
+    view2: {
+        width: "92%",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 10,
+        flexDirection: 'row',
+
+    },
+    btn: {
+        padding: 10,
+        borderRadius: 25,
+        backgroundColor: "#0B3270",
+        justifyContent: "center",
+        alignItems: "center",
+        marginRight: 10
+    },
+    btn2: {
+        padding: 5,
+        height: 100,
+        width: 250,
+        borderRadius: 25,
+        backgroundColor: "#0B3270",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    text: {
+        fontSize: 20,
+        fontWeight: "600",
+        color: "#fff",
+        padding: 10
+    },
+    listtext: {
+        paddingLeft: 20,
+        fontSize: 20,
+        fontWeight: "600",
+        color: "black"
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 22,
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        padding: 35,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+    }
+})
