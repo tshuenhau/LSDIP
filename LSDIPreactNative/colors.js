4aebc0c6e00b82069437bee41609e8f97b8402eb
seancxy <seanchengxy13@gmail.com>
2023-03-15 00:06:25 +0800

    basic functionality of order pickup

--- LSDIPreactNative/colors.js
+++ LSDIPreactNative/colors.js
@@ -4,7 +4,7 @@ export default {
     gray: '#C5C5C7',
     mediumGray: '#F6F7FB',
     lightGray: '#FAFAFA',
-    shadowGray: '#000000',
+    shadowGray: '#595953',
     dismissBlue: '#344869',
     darkBlue: '#0B3270',
     lightBlue: '#69ACF5',
